7ce52b60021749a263baf6a2059738cf6bb06598
vighnesh-kargutkar <106871029+vighnesh-kargutkar@users.noreply.github.com>
2023-07-14 07:16:03 +0530

    Add files via upload

---
+++ prac7uog/prac7uog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prac7uog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ prac8upg/prac8upg/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prac8upg.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
